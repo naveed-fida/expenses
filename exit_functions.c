d7ea6fc5ced839dceaa82e3182b484e43f9907b0
Naveed Fida <naveedfida01@gmail.com>
2021-11-08 02:34:23 +0500

    add more exit conditions

--- exit_functions.c
+++ exit_functions.c
@@ -7,7 +7,8 @@ void exit_invalid_command(char *cmd)
     exit(1);
 }
 
-void exit_invalid_opt_expense(char *token) {
+void exit_invalid_opt_expense(char *token)
+{
     fprintf(
         stderr,
         "Error: Ivalid Syntax. "
@@ -18,17 +19,44 @@ void exit_invalid_opt_expense(char *token) {
     exit(1);
 }
 
-void exit_invalid_value(char *opt, char *val) {
+void exit_invalid_value(char *opt, char *val)
+{
     fprintf(stderr, "Error: `%s` is not a valid value for option `%s`\n", val, opt);
     exit(1);
 }
 
-void exit_redundant_option(char *opt) {
+void exit_redundant_option(char *opt)
+{
     fprintf(stderr, "Error: %s is a redundant option.\n", opt);
     exit(1);
 }
 
-void exit_no_expense() {
+void exit_no_expense()
+{
     fprintf(stderr, "Error: No expense amount given.\n");
+    exit(1);
+}
+
+void exit_invalid_time_options(char *cmd)
+{
+    fprintf(stderr, "Error: Can't use time options (-d, -m, -y) with `%s`\n", cmd);
+    exit(1);
+}
+
+void exit_value_too_long(char *opt, int max_len)
+{
+    fprintf(
+        stderr,
+        "Error: Value too long. "
+        "A %s's length, can't be longer than %i\n",
+        opt,
+        max_len
+    );
+    exit(1);
+}
+
+void exit_no_value(char *opt)
+{
+    fprintf(stderr, "Error: No value provided for `%s`\n", opt);
     exit(1);
 }
--- include/exit_functions.h
+++ include/exit_functions.h
@@ -6,5 +6,8 @@ void exit_invalid_opt_expense(char *token);
 void exit_invalid_value(char *opt, char *val);
 void exit_redundant_option(char *opt);
 void exit_no_expense();
+void exit_invalid_time_options(char *cmd);
+void exit_value_too_long(char *opt, int max_len);
+void exit_no_value(char *opt);
 
 #endif
