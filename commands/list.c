13429bbc9c1ab6f176dcd03035bec44e688c7d32
Naveed Fida <naveedfida01@gmail.com>
2021-11-09 15:58:46 +0500

    add filtering by tag and category feature to list command

--- commands/list.c
+++ commands/list.c
@@ -1,4 +1,3 @@
-#include <string.h>
 #include <stdio.h>
 #include <stdbool.h>
 #include <stdlib.h>
@@ -8,22 +7,42 @@
 #include "common.h"
 #include "exit_functions.h"
 
+#define _GNU_SOURCE
+#include <string.h>
+
 bool file_exists(char *path)
 {
     return access(path, F_OK) == 0;
 }
 
-void format_and_print_row(char *line)
+bool cat_matches(char *row_cat, char *opt_cat)
+{
+    return opt_cat == '\0' || strcasestr(row_cat, opt_cat) != NULL;
+}
+
+bool tag_matches(char *row_tag, char *opt_tag)
+{
+    return opt_tag == '\0' || strcasestr(row_tag, opt_tag) != NULL;
+}
+
+void format_and_print_row(char *line, struct arguments *arg_struct)
 {
     char category[MAX_CAT_LEN + 5], tag[MAX_TAG_LEN + 5], date_string[18];
     float expense_amount;
     struct tm *time_struct;
     time_t timestamp;
 
-    sscanf(line, "%li%f%s%s", &timestamp, &expense_amount, category, tag);
+    sscanf(line, "%li %f \"%[^\"]\" \"%[^\"]\"", &timestamp, &expense_amount, category, tag);
+
+    if (
+        !cat_matches(category, arg_struct->options.cat) ||
+        !tag_matches(tag, arg_struct->options.tag)
+    )
+        return;
+    
     time_struct = localtime(&timestamp);
     strftime(date_string, (size_t) 30, "%a, %d/%m/%y", time_struct);
-    printf("%s\t%.2f\t%s\t%s\n", date_string, expense_amount, category, tag);
+    printf("%-25s%-15.2f%-30s%-30s\n", date_string, expense_amount, category, tag);
 }
 
 void retrieve_list(struct arguments *arg_struct)
@@ -34,7 +53,6 @@ void retrieve_list(struct arguments *arg_struct)
     if (!file_exists(expenses_data_path))
         exit_no_data_file();
 
-    printf("Date Created\tExpense Amount\tCategory\tTag\n");
     FILE *fp;
     if ((fp = fopen(expenses_data_path, "r")) == NULL)
         exit_cant_open_data_file();
@@ -42,8 +60,9 @@ void retrieve_list(struct arguments *arg_struct)
     char *row           = (char *) malloc(MAX_TAG_LEN + MAX_CAT_LEN + 80);
     size_t size;
 
+    printf("%-25s%-15s%-30s%-30s\n", "Date", "Amount", "Category", "Tag");
     while(getline(&row, &size, fp) != -1)
-        format_and_print_row(row);
+        format_and_print_row(row, arg_struct);
 
     free(row);
 }
